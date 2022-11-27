c21e6ff6c5a1d01a8143586b698429b286e2ccca
rijvan00 <rijvanahmad1382001@gmail.com>
2022-11-27 21:24:00 +0530

    socket io server implemented

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,39 +1,40 @@
-import { ErrorHandler} from "../utils/errorhandler";
+import  ErrorHandler from "../utils/ErrorHandler";
 
 
-class Error{
-    static  Errors = (err:any, req:any, res:any, next:any) => {
-  err.statusCode = err.statusCode || 500;
-  err.message = err.message;
+function Error(error:any, req:any, res:any, next:any){
+  error.statusCode =error.statusCode || 500;
+  error.message = error.message || "Internal Server Error";
   //wrong mongoose object id error'
-  if (err.name === "CastError") {
-    const message = `${err.path}`;
+  if (error.name === "CastError") {
+    const message = `Resource not found. Invalid:  ${error.path}`;
     console.log(message);
-    err = new ErrorHandler(message, 400);
+    error = new ErrorHandler(message, 400);
   }
 
   //duplicate key error
-  if (err.code === 11000) {
-    const message = `${Object.keys(err.keyValue)} `;
-    err = new ErrorHandler(message, 400);
+  if (error.code == 11000) {
+    const message = `This ${Object.keys(error.keyValue)} is already registered with other account`;
+    error = new ErrorHandler(message, 400);
   }
 
  //wrong jwt error
-  if (err.name === "JsonWebTokenError") {
+  if (error.name === "JsonWebTokenError") {
     const message = "Your url is invalid. Try again!!!";
-    err = new ErrorHandler(message, 400);
+    error = new ErrorHandler(message, 400);
   }
  //token expired error
-  if (err.name === "TokenExpiredError") {
+  if (error.name === "TokenExpiredError") {
     const message = "Your url is expired, PLease Try Again ";
-    err = new ErrorHandler(message, 400);
+    error = new ErrorHandler(message, 400);
   }
-  res.status(err.status).json({
+
+
+  res.status(error.statusCode).json({
     success: false,
-    message: err.message,
+    message: error.message,
   });
 }
-}
+
 
 export default Error;
 
