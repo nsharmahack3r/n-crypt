65844300ae33ea314eb9a0e7e02f9a2c0d337394
rijvan00 <rijvanahmad1382001@gmail.com>
2022-12-30 15:25:29 +0530

    ErrorHandler removed

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,40 +1,40 @@
-import  ErrorHandler from "../utils/ErrorHandler";
+// import  ErrorHandler from "../utils/ErrorHandler";
 
 
-function Error(error:any, req:any, res:any, next:any){
-  error.statusCode =error.statusCode || 500;
-  error.message = error.message || "Internal Server Error";
-  //wrong mongoose object id error'
-  if (error.name === "CastError") {
-    const message = `Resource not found. Invalid:  ${error.path}`;
-    console.log(message);
-    error = new ErrorHandler(message, 400);
-  }
+// function Error(error:any, req:any, res:any, next:any){
+//   error.statusCode =error.statusCode || 500;
+//   error.message = error.message || "Internal Server Error";
+//   //wrong mongoose object id error'
+//   if (error.name === "CastError") {
+//     const message = `Resource not found. Invalid:  ${error.path}`;
+//     console.log(message);
+//     error = new ErrorHandler(message, 400);
+//   }
 
-  //duplicate key error
-  if (error.code == 11000) {
-    const message = `This ${Object.keys(error.keyValue)} is already registered with other account`;
-    error = new ErrorHandler(message, 400);
-  }
+//   //duplicate key error
+//   if (error.code == 11000) {
+//     const message = `This ${Object.keys(error.keyValue)} is already registered with other account`;
+//     error = new ErrorHandler(message, 400);
+//   }
 
- //wrong jwt error
-  if (error.name === "JsonWebTokenError") {
-    const message = "Your url is invalid. Try again!!!";
-    error = new ErrorHandler(message, 400);
-  }
- //token expired error
-  if (error.name === "TokenExpiredError") {
-    const message = "Your url is expired, PLease Try Again ";
-    error = new ErrorHandler(message, 400);
-  }
-
-
-  res.status(error.statusCode).json({
-    success: false,
-    message: error.message,
-  });
-}
+//  //wrong jwt error
+//   if (error.name === "JsonWebTokenError") {
+//     const message = "Your url is invalid. Try again!!!";
+//     error = new ErrorHandler(message, 400);
+//   }
+//  //token expired error
+//   if (error.name === "TokenExpiredError") {
+//     const message = "Your url is expired, PLease Try Again ";
+//     error = new ErrorHandler(message, 400);
+//   }
 
 
-export default Error;
+//   res.status(error.statusCode).json({
+//     success: false,
+//     message: error.message,
+//   });
+// }
+
+
+// export default Error;
 
--- src/utils/errorhandler.ts
+++ src/utils/errorhandler.ts
@@ -1,11 +1,11 @@
-class ErrorHandler extends Error {
-  statusCode: number;
+// class ErrorHandler extends Error {
+//   statusCode: number;
 
-  constructor(message: string, statusCode: number) {
-    super(message);
+//   constructor(message: string, statusCode: number) {
+//     super(message);
 
-    this.statusCode = statusCode;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-export default ErrorHandler;
+//     this.statusCode = statusCode;
+//     Error.captureStackTrace(this, this.constructor);
+//   }
+// }
+// export default ErrorHandler;
